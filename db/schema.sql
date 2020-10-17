3bf01480dd4fdb9e8dcd292e7cf272ad4da227ea
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-10-17 02:42:12 -0600

    added routes for votes

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,7 @@
-
-
-
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS votes;
 
 
 CREATE TABLE parties (
@@ -12,17 +10,13 @@ CREATE TABLE parties (
     description TEXT
 );
 
-
 CREATE TABLE candidates (
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     industry_connected BOOLEAN NOT NULL,
     party_id INTEGER UNSIGNED,
-    -- Allows us to flag the party_id field as on official foreign key and tells SQL which table and field it references. 
-    -- Ensures that no ID can be inserted into the candidates table if it doesnt also exist in the parties table.
     CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
-    -- ON DELETE SET NULL makes sure that if a party is deleted, it updates accordingly on candidates. If a party is deleted, it sets the candidate's party_id to null.
 );
 
 CREATE TABLE voters (
@@ -31,4 +25,14 @@ CREATE TABLE voters (
     last_name VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NUll,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE votes (
+    id INTEGER PRIMARY KEY,
+    voter_id INTEGER UNSIGNED NOT NULL,
+    candidate_id INTEGER UNSIGNED NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT uc_voter UNIQUE (voter_id),
+    CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+    CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
